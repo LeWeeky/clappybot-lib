c504f99147695a707d68cfac859d6cec5b4b0693
LeWeeky <leweeky@gmail.com>
2025-07-04 09:49:20 +0200

    feat: preparation for reinstatement of the permissions system

---
+++ dist/libraries/permissions/permissions.js
@@ -0,0 +1,14 @@
+class CBPermissions
+{
+	static Owner = 1
+	static Admin = 2;
+	static Edition = 4
+	static Ban = 8
+	static Kick = 16
+	static Mute = 32
+	
+}
+
+module.exports = {
+	CBPermissions
+}
---
+++ dist/models/Permissions.js
@@ -0,0 +1,44 @@
+const { AModel } = require("clappybot/dist/libraries/models/AModel");
+const { CBPermissions } = require("clappybot/dist/libraries/permissions/permissions");
+
+class APermissions extends AModel
+{
+	static fields = {
+		user_id: 'string',
+		bits: 'integer'
+	};
+	add(permission_bit)
+	{
+		this.bits = this.bits | permission_bit;
+	}
+	remove(permission_bit)
+	{
+		this.bits = this.bits - permission_bit;
+	}
+	has(permission_bit)
+	{
+		return (
+			this.bits && CBPermissions.Owner
+			|| this.bits && permission_bit
+		);
+	}
+	isAdmin()
+	{
+		return (this.bits && CBPermissions.Admin);
+	}
+}
+
+class GuildPermissions extends APermissions
+{
+	static table = 'guild_permissions';
+}
+
+class GlobalPermissions extends APermissions
+{
+	static table = 'global_permissions';
+}
+
+module.exports = {
+	GuildPermissions,
+	GlobalPermissions
+}
